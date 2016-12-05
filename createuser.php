84e1207d505cddc990ae030d515c8a7dbfa33447
Tony <antony.momanyi@gmail.com>
2016-12-05 09:54:12 +0300

    error fixing

--- createuser.php
+++ createuser.php
@@ -20,162 +20,27 @@ $defaultPassword = 123456;
 
 //push values into db using query
 
-$sql = "INSERT INTO `Users` (`UserID`, `FirstName`, `LastName`, `Category`, `Username`, `Password`) VALUES (NULL, '$fname', '$lname', '$category', '$usrname', '$defaultPassword')";
-
-if ($con->query($sql)=== TRUE){
-  $successfulMsg = "User created";
-  echo "<script type = 'text/javascript'>alert('$successfulMsg');</script>";
+//check if username is taken
+$sqlcheck = "SELECT UserID FROM Users WHERE Username = '$usrname'";
+$result = $con->query($sqlcheck);
+if($result->num_rows > 0){
+  $row = $result->fetch_assoc();
+  $userexistserror = "That username is taken";
+  header('Location: createUser.html');
+  echo "That username is taken";
+  $UserID = $row['UserID'];
 }else{
-  $errorMsg = "An error occured";
-  echo "Error: " . $sql . "<br>" . $con->error;
-  echo "<script type = 'text/javascript'>alert('$errorMsg');</script>";
+  $sqlinsert = "INSERT INTO `Users` (`UserID`, `FirstName`, `LastName`, `Category`, `Username`, `Password`) VALUES (NULL, '$fname', '$lname', '$category', '$usrname', '$defaultPassword')";
+  if ($con->query($sqlinsert)=== TRUE){
+    $successfulMsg = "User created";
+    echo "<script type = 'text/javascript'>alert('$successfulMsg');</script>";
+    header('Location: viewusers.php');
+  }else{
+    $errorMsg = "An error occured";
+    echo "Error: " . $sqlinsert . "<br>" . $con->error;
+    echo "<script type = 'text/javascript'>alert('$errorMsg');</script>";
+    header('Location: createuser.php');
+  }
 }
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Create User</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Bootstrap -->
-	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<!-- styles -->
-	<link href="css/styles.css" rel="stylesheet">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-	<![endif]-->
-</head>
-<body>
-	<div class="header">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-5">
-					<!-- Logo -->
-					<div class="logo">
-						<h1><a href="index.html">Farm Manager's Portal</a></h1>
-					</div>
-				</div>
-				<div class="col-md-5">
-
-				</div>
-				<div class="col-md-2">
-					<div class="navbar navbar-inverse" role="banner">
-						<nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
-							<ul class="nav navbar-nav">
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown">My Account <b class="caret"></b></a>
-									<ul class="dropdown-menu animated fadeInUp">
-										<li><a href="profile.html">Profile</a></li>
-										<li><a href="login.html">Logout</a></li>
-									</ul>
-								</li>
-							</ul>
-						</nav>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="page-content">
-		<div class="row">
-			<div class="col-md-2">
-				<div class="sidebar content-box" style="display: block;">
-					<ul class="nav">
-						<!-- Main menu -->
-						<li class="current"><a href="index.html"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
-						<li class="submenu">
-							<a href="#">
-								<i class="glyphicon glyphicon-list"></i> Users
-								<span class="caret pull-right"></span>
-							</a>
-							<!-- Sub menu -->
-							<ul>
-								<li><a href="viewusers.php">View Users</a></li>
-								<li><a href="createUser.html">Create New</a></li>
-							</ul>
-						</li>
-						<li class="submenu">
-							<a href="#">
-								<i class="glyphicon glyphicon-list"></i> Reports
-								<span class="caret pull-right"></span>
-							</a>
-							<!-- Sub menu -->
-							<ul>
-								<li><a href="mortalityReport.html">Mortality</a></li>
-								<li><a href="feedConsumptionReport.html">Feed Consumption</a></li>
-								<li><a href="signup.html">Expenditure</a></li>
-							</ul>
-						</li>
-						<li class="submenu">
-							<a href="#">
-								<i class="glyphicon glyphicon-list"></i> Forms
-								<span class="caret pull-right"></span>
-							</a>
-							<!-- Sub menu -->
-							<ul>
-								<li><a href="mortality.html">Mortality</a></li>
-								<li><a href="feedConsumption.html">Feed Consumption</a></li>
-								<li><a href="expenditure.html">Expenditure</a></li>
-							</ul>
-						</li>
-
-					</ul>
-				</div>
-			</div>
-
-
-
-			<div class="col-md-6">
-				<div class="content-box-large">
-					<div class="panel-heading">
-						<div class="panel-title">New User Details</div>
-					</div>
-					<div class="panel-body">
-						<form action ="createuser.php" method = "post" name = "newuser" required>
-							<fieldset>
-								<div class="form-group">
-									<label>First Name</label>
-									<input class ="form-control" type="text" name="firstname" required>
-								</div>
-								<div class="form-group">
-									<label>Last Name</label>
-									<input class="form-control" type="text" name="lastname" required>
-								</div>
-								<div class="form-group">
-									<label>Username</label>
-									<input class="form-control" type="text" name="username" required>
-								</div>
-								<div class = "form-group">
-									<label>Category</label>
-									<p>
-										<select class="selectpicker" name="category" required>
-											<option value="Manager">Manager</option>
-											<option value="Worker">Worker</option>
-										</select>
-									</p>
-								</div>
-							</fieldset>
-							<input type="submit" class = "btn btn-primary signup" value="Create" ></tab>
-						</form>
-					</div>
-				</div>
-			</div>
-
-
-		</div>
-	</div>
-
-
-
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="https://code.jquery.com/jquery.js"></script>
-	<!-- Include all compiled plugins (below), or include individual files as needed -->
-	<script src="bootstrap/js/bootstrap.min.js"></script>
-	<script src="js/custom.js"></script>
-</body>
-</html>
